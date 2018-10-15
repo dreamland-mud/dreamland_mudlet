5add8af53d78b790d930e1c4d8bcc819fa6f737a
Ruffina Koza <39081962+ruffinakoza@users.noreply.github.com>
2018-10-16 00:45:27 +1300

    Fix path to travis deploy key.

--- deploy.sh
+++ deploy.sh
@@ -35,25 +35,9 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
+chmod 600 ../deploy_key
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add ../deploy_key
 
 git push $SSH_REPO $TARGET_BRANCH
-
-
-#zip dl-new.zip Dreamland.xml config.lua
-#SHA_NEW=`sha1sum -b dl-new.zip | cut -f1 -d " "`
-#wget -O "dl-old.zip" https://dreamland-mud.github.io/dreamland_mudlet/downloads/dl.zip
-#SHA_OLD=`sha1sum -b dl-old.zip | cut -f1 -d " "`
-#ls -la
-#echo "SHA_NEW $SHA_NEW"
-#echo "SHA_OLD $SHA_OLD"
-#
-#if [ "$SHA_NEW" = "$SHA_OLD" ]; then
-#    echo "No changes, skipping deploy."
-#    exit 0
-#fi    
-
-
