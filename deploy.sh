4c37f2737447d2415f18d9b81824355e03b84735
Ruffina Koza <39081962+ruffinakoza@users.noreply.github.com>
2018-10-16 00:51:55 +1300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -33,8 +33,10 @@ git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+echo "KEY_VAR $ENCRYPTED_KEY_VAR IV_VAR $ENCRYPTED_IV_VAR"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+echo "KEY $ENCRYPTED_KEY IV $ENCRYPTED_IV"
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
 chmod 600 ../deploy_key
 eval `ssh-agent -s`
