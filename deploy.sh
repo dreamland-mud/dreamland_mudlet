a04a54b9469329fbdea7f47db425f478dd351cf0
Ruffina Koza <39081962+ruffinakoza@users.noreply.github.com>
2018-10-15 23:07:55 +1300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy."
+    echo "Not on master, skipping deploy."
     exit 0
 fi
 
@@ -14,12 +14,18 @@ REPO=`git config remote.origin.url`
 SHA=`git rev-parse --verify HEAD`
 echo "Repository: $REPO"
 echo "SHA: $SHA"
-pwd
-ls -a
-ls $HOME
-exit 0
 
-#TEMPDIR="$HOME/tmp/" 
-#mkdir -p "$TEMPDIR"
-#wget -O "$TEMPDIR/dl-current.zip" https://dreamland-mud.github.io/dreamland_mudlet/downloads/dl.zip
+zip dl-new.zip Dreamland.xml config.lua
+SHA_NEW=`sha1sum -b dl-new.zip | cut -f1 -d " "`
+wget -O "dl-old.zip" https://dreamland-mud.github.io/dreamland_mudlet/downloads/dl.zip
+SHA_OLD=`sha1sum -b dl-old.zip | cut -f1 -d " "`
+ls -la
+echo "SHA_NEW $SHA_NEW"
+echo "SHA_OLD $SHA_OLD"
+
+if [ "$SHA_NEW" = "$SHA_OLD" ]; then
+    echo "No changes, skipping deploy."
+    exit 0
+fi    
+
 
